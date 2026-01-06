d1480d034ba8e53f4c1e9dfee74f30a7b9e784a8
Yash <yashdesh095@gmail.com>
2026-01-06 10:16:20 +0530

    fix: removed unwanted files

--- prisma/migrations/20260101174815_add_rate_limit_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `rateLimitType` on the `SourcingJob` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "SourcingJob" DROP COLUMN "rateLimitType",
-ADD COLUMN     "rateLimitService" TEXT;
